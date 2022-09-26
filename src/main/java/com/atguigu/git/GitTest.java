d23f19a9a701477c9e1b4e3709d8220d69156c2c
Wu <841866952@qq.com>
2022-09-26 09:09:29 +0800

    新建文件

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author wjp
+ * @date 2022/9/26 9:04
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("新建文件");
+    }
+}
